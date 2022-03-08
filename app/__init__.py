3b8083ed6ea9098cccf0d54650719f4c1737c0e2
sagar-alande <sa274@njit.edu>
2022-03-07 19:40:32 -0500

    Updated workflow name and linkand HTML files

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,29 @@
 """A simple flask web app"""
 from flask import Flask, render_template
-from app.context_processors import utility_text_processors
-from app.simple_pages import simple_pages
 
 
 def create_app():
     """Create and configure an instance of the Flask application."""
     app = Flask(__name__)
     app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-    app.register_blueprint(simple_pages)
-    app.context_processor(utility_text_processors)
 
+    @app.route("/")
+    def index():
+        return render_template('index.html')
+
+    @app.route("/Bdata")
+    def about():
+        return render_template('bigdata.html')
+
+    @app.route("/DBMS")
+    def fpat():
+        return render_template('DBMS.html')
+
+    @app.route("/python")
+    def python():
+        return render_template('python.html')
+
+    @app.route("/R-prog")
+    def rprog():
+        return render_template('rprog.html')
     return app
